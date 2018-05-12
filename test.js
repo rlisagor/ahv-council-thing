61eeb59b551eac551f34bb746c00de59c816b2be
Reilly <reilly.wood@icloud.com>
2018-05-11 22:11:40 -0700

    Fix test syntax (it happened to work before, but type checker didn't like it)

--- test.js
+++ test.js
@@ -7,23 +7,28 @@ function compareExpectedAndActual(input, expected) {
 
 describe('slackHelper.extractEmailAddress()', function () {
     describe('wrapped', function () {
-        it('should work when Slack wraps addresses in a <mailto:>',
-            compareExpectedAndActual('<mailto:john.smith@icloud.com|john.smith@icloud.com>', 'john.smith@icloud.com'));
+        it('should work when Slack wraps addresses in a <mailto:>', function () {
+            compareExpectedAndActual('<mailto:john.smith@icloud.com|john.smith@icloud.com>', 'john.smith@icloud.com');
+        });
     });
     describe('wrappedWithoutName', function () {
-        it('should work when Slack wraps addresses in a <mailto:>, even if they stop including a name after a pipe',
-            compareExpectedAndActual('<mailto:john.smith@icloud.com>', 'john.smith@icloud.com'));
+        it('should work when Slack wraps addresses in a <mailto:>, even if they stop including a name after a pipe', function () {
+            compareExpectedAndActual('<mailto:john.smith@icloud.com>', 'john.smith@icloud.com');
+        });
     });
     describe('wrappedWithSpaceInName', function () {
-        it('should work when Slack wraps addresses in a <mailto:> with a name inside',
-            compareExpectedAndActual('<mailto:john.smith@icloud.com|John Smith>', 'john.smith@icloud.com'));
+        it('should work when Slack wraps addresses in a <mailto:> with a name inside', function () {
+            compareExpectedAndActual('<mailto:john.smith@icloud.com|John Smith>', 'john.smith@icloud.com');
+        });
     });
     describe('wrappedWithWhitespace', function () {
-        it('should work even if there is whitespace around the <mailto:> tag',
-            compareExpectedAndActual('  <mailto:john.smith@icloud.com|john.smith@icloud.com> ', 'john.smith@icloud.com'));
+        it('should work even if there is whitespace around the <mailto:> tag', function () {
+            compareExpectedAndActual('  <mailto:john.smith@icloud.com|john.smith@icloud.com> ', 'john.smith@icloud.com');
+        });
     });
     describe('unwrapped', function () {
-        it('should work even if Slack stops wrapping values',
-            compareExpectedAndActual('john.smith@icloud.com', 'john.smith@icloud.com'));
+        it('should work even if Slack stops wrapping values', function () {
+            compareExpectedAndActual('john.smith@icloud.com', 'john.smith@icloud.com');
+        });
     });
 });

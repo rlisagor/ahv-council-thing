12af89e82f8ca6c88ba87b0b8e7ee04e13760952
Roman Lisagor <rlisagor@gmail.com>
2020-01-23 11:11:32 -0800

    Oops. I missed some code when adding support for Mailgun. Adding it now.

--- src/providerWrapper.js
+++ src/providerWrapper.js
@@ -4,15 +4,40 @@
 // Feel free to use this layer for other purposes (auth, logging?) too
 // TODO: move all AWS calls into this wrapper and integration-test them
 
-let nodemailer = require('nodemailer');
-let aws = require('aws-sdk');
+const nodemailer = require('nodemailer');
+const aws = require('aws-sdk');
+const mg = require('nodemailer-mailgun-transport');
 
 module.exports = class ProviderWrapper {
   static async sendEmail(emailOptions) {
-    let transporter = nodemailer.createTransport({
+    let transporter = this.createTransport();
+    await transporter.sendMail(emailOptions);
+  }
+
+  static createTransport() {
+    switch (process.env.MAIL_PROVIDER) {
+    case 'ses':
+    case undefined:
+      return this.createSESTransport();
+    case 'mailgun':
+      return this.createMailgunTransport();
+    default:
+      throw new Error(`invalid provider: ${process.env.MAIL_PROVIDER}`);
+    }
+  }
+
+  static createSESTransport() {
+    return nodemailer.createTransport({
       SES: new aws.SES({ apiVersion: '2010-12-01' })
     });
+  }
 
-    await transporter.sendMail(emailOptions);
+  static createMailgunTransport() {
+    return nodemailer.createTransport(mg({
+      auth: {
+        api_key: process.env.MAILGUN_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+      }
+    }));
   }
 };
